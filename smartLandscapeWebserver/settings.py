40e2993d9c43378bb6d5e4491896babd833a98f0
winar-jin <winar1994@outlook.com>
2018-04-13 15:37:39 +0800

    [jin] delete sqllite file

--- smartLandscapeWebserver/settings.py
+++ smartLandscapeWebserver/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'smartLandscapeWebserver.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'test',
+        'USER': 'admin',
+        'PASSWORD': 'smartLandscape',
+        'HOST': '59.110.221.56',
+        'PORT': '3306',
     }
 }
 
